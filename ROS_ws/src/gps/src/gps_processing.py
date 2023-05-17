30d8d3cbd19e171f222f4621d81e71fbef9e39e8
danielauu <doacuna@uc.cl>
2023-05-17 16:43:19 +0200

    tested callbacks

--- ROS_ws/src/gps/src/gps_processing.py
+++ ROS_ws/src/gps/src/gps_processing.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
 from numpy import mean, arctan2
 from std_msgs.msg import Empty, Bool
 from sensor_msgs.msg import NavSatFix
 from geometry_msgs.msg import Pose2D, Quaternion
 from suncalc import get_position
-from sprinter_srvs import GetOrientation, GetOrientationResponse
+#from sprinter_srvs import GetOrientation, GetOrientationResponse
 from datetime import datetime
 from collections import deque
 
@@ -16,7 +16,7 @@ class GPSProcessing:
         self.not_reached = True
         # Data measured
         self.q = deque()
-        self.qsize = 100
+        self.qsize = 5
         self.curr_pos = []
         # Subscribers
         self.sub_global = rospy.Subscriber("/gps/robot_global_orientation/cmd", Empty, self.callback_global)
@@ -24,21 +24,22 @@ class GPSProcessing:
         self.sub_reached = rospy.Subscriber("/target/pose/reached", Bool, self.callback_reached)
         # Publishers
         self.pub_global = rospy.Publisher("/gps/robot_global_orientation/done", Bool, queue_size=1)
-        self.pub_pos = rospy.Publisher("/tf_static", NavSatFix, queue_size=1)
+        self.pub_pos = rospy.Publisher("/tf_static", Pose2D, queue_size=1)
         self.pub = rospy.Publisher("/target/pose/cmd", Pose2D, queue_size=1)
         # Services
-        self.sun_server = rospy.Service('(/gps/get_sun_orientation)', GetOrientation, self.handle_get_sun_orientation)
+        #self.sun_server = rospy.Service('(/gps/get_sun_orientation)', GetOrientation, self.handle_get_sun_orientation)
         rospy.spin()
 
-    def handle_get_sun_orientation(self, req):
-        date = datetime.now()
-        pos = get_position(date, self.curr_pos[1], self.curr_pos[0])
-        print("Returning [%s, %s]"%(pos[0], pos[1]))
-        return GetOrientationResponse(Quaternion(x=pos[0], y=0, z=pos[1], w=0))
+    # def handle_get_sun_orientation(self, req):
+    #     date = datetime.now()
+    #     pos = get_position(date, self.curr_pos[1], self.curr_pos[0])
+    #     print("Returning [%s, %s]"%(pos[0], pos[1]))
+    #     return GetOrientationResponse(Quaternion(x=pos[0], y=0, z=pos[1], w=0))
     
     def callback_gps(self, data):
-        self.q.append([data.status.latitude, data.status.longitude])
+        self.q.append([data.latitude, data.longitude])
         if(len(self.q) > self.qsize):
+            rospy.loginfo('Ready')
             self.q.popleft()
 
     def measure_position(self):
@@ -47,20 +48,24 @@ class GPSProcessing:
         return Pose2D(x=pos[0], y=pos[1])
     
     def callback_reached(self, data):
-        if data:
-            self.is_reached = False
+        rospy.loginfo(data)
+        if data.data:
+            self.not_reached = False
 
-    def callback_global(self):
+    def callback_global(self, data):
         if self.is_done:
             self.pub_global.publish(Bool(data=True))
         else:
-            first = self.measure_position(self)
+            first = self.measure_position()
             self.pub.publish(first)
+            rate = rospy.Rate(1) # 0.1hz
             while self.not_reached:
-                pass
-            second = self.measure_position(self)
+                rospy.loginfo(first)
+                rate.sleep()
+            second = self.measure_position()
+            rospy.loginfo('second!')
             north_angle = arctan2(second.x - first.x, second.y - first.y)
-            self.pub_pos.publish(Pose2D(second.x,second.y, north_angle))
+            self.pub_pos.publish(Pose2D(x=second.x,y=second.y, theta=north_angle))
             self.is_done = True
 
 
