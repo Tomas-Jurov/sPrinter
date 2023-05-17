0c1077e962841144e7c9746dc69874fa6015ceeb
danielauu <doacuna@uc.cl>
2023-05-18 01:28:27 +0200

    service

--- ROS_ws/src/gps/src/gps_processing.py
+++ ROS_ws/src/gps/src/gps_processing.py
@@ -5,7 +5,8 @@ from std_msgs.msg import Empty, Bool
 from sensor_msgs.msg import NavSatFix
 from geometry_msgs.msg import Pose2D, TransformStamped
 from suncalc import get_position
-#from sprinter_srvs import GetOrientation, GetOrientationResponse
+from sprinter_srvs import GetOrientation, GetOrientationResponse
+from datetime import datetime
 from collections import deque
 from tf2_ros import StaticTransformBroadcaster
 import tf
@@ -27,14 +28,14 @@ class GPSProcessing:
         self.pub_global = rospy.Publisher("/gps/robot_global_orientation/done", Bool, queue_size=1)
         self.pub = rospy.Publisher("/target/pose/cmd", Pose2D, queue_size=1)
         # Services
-        #self.sun_server = rospy.Service('(/gps/get_sun_orientation)', GetOrientation, self.handle_get_sun_orientation)
+        self.sun_server = rospy.Service('(/gps/get_sun_orientation)', GetOrientation, self.handle_get_sun_orientation)
         rospy.spin()
 
-    # def handle_get_sun_orientation(self, req):
-    #     date = datetime.now()
-    #     pos = get_position(date, self.curr_pos[1], self.curr_pos[0])
-    #     print("Returning [%s, %s]"%(pos[0], pos[1]))
-    #     return GetOrientationResponse(Quaternion(x=pos[0], y=0, z=pos[1], w=0))
+    def handle_get_sun_orientation(self, req):
+        date = datetime.now()
+        pos = get_position(date, self.curr_pos[1], self.curr_pos[0])
+        rospy.loginfo("Returning [%s, %s]"%(pos[0], pos[1]))
+        return GetOrientationResponse()
     
     def callback_gps(self, data):
         self.q.append([data.latitude, data.longitude])
