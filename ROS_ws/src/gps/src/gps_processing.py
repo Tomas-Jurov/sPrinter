700c6a82262470c1502167bba5f859199916c0c7
danielauu <doacuna@uc.cl>
2023-05-18 10:22:42 +0200

    without services

--- ROS_ws/src/gps/src/gps_processing.py
+++ ROS_ws/src/gps/src/gps_processing.py
@@ -28,14 +28,14 @@ class GPSProcessing:
         self.pub_global = rospy.Publisher("/gps/robot_global_orientation/done", Bool, queue_size=1)
         self.pub = rospy.Publisher("/target/pose/cmd", Pose2D, queue_size=1)
         # Services
-        self.sun_server = rospy.Service('(/gps/get_sun_orientation)', GetOrientation, self.handle_get_sun_orientation)
+        # self.sun_server = rospy.Service('(/gps/get_sun_orientation)', GetOrientation, self.handle_get_sun_orientation)
         rospy.spin()
 
-    def handle_get_sun_orientation(self, req):
-        date = datetime.now()
-        pos = get_position(date, self.curr_pos[1], self.curr_pos[0])
-        rospy.loginfo("Returning [%s, %s]"%(pos[0], pos[1]))
-        return GetOrientationResponse()
+    # def handle_get_sun_orientation(self, req):
+    #     date = datetime.now()
+    #     pos = get_position(date, self.curr_pos[1], self.curr_pos[0])
+    #     rospy.loginfo("Returning [%s, %s]"%(pos[0], pos[1]))
+    #     return GetOrientationResponse()
     
     def callback_gps(self, data):
         self.q.append([data.latitude, data.longitude])
