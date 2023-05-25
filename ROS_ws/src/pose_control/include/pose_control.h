ef44f1bda808af579a6c8e4511b70da40383cf7c
Patrik Knaperek <pknaperek@gmail.com>
2023-05-25 20:14:06 +0200

    Pose Control refractor
    - added stop callback
    - added launch file and param file
    - updated README
    - rubbish cleaned...
    - simply pure pursuit implemented

--- ROS_ws/src/pose_control/include/pose_control.h
+++ ROS_ws/src/pose_control/include/pose_control.h
@@ -1,28 +1,66 @@
 #pragma once
 
 #include <ros/ros.h>
+#include <tf/tf.h>
+#include <std_msgs/Empty.h>
 #include <std_msgs/Bool.h>
+#include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Pose2D.h>
 #include <geometry_msgs/Twist.h>
-#include <nav_msgs/Odometry.h>
+#include <tf/transform_datatypes.h>
+
+namespace PoseControlNS
+{
+struct Params
+{
+  float max_lin_speed;
+  float max_ang_speed;
+  float target_position_tolerance;
+  float target_orientation_tolerance;
+  float look_ahead_distance_;
+};
 
 class PoseControl
 {
 public:
-  PoseControl(const ros::Publisher& target_reached_pub, const ros::Publisher& cmd_vel_pub);
+  PoseControl(const ros::Publisher& cmd_vel_pub, const ros::Publisher& target_reached_pub);
   ~PoseControl() = default;
 
   void update();
+  void loadParams(const ros::NodeHandle& nh);
 
   // Callbacks
-  void targetCmdCallback(const geometry_msgs::Pose2D::ConstPtr& msg);
-  void odomCallback(const nav_msgs::Odometry::ConstPtr& msg);
+  void targetCmdCallback(const geometry_msgs::Pose2D::ConstPtr &msg);
+  void odomCallback(const nav_msgs::Odometry::ConstPtr &msg);
+  void stopCallback(const std_msgs::Empty::ConstPtr &msg);
 
 private:
-  ros::Publisher target_reached_pub_, cmd_vel_pub_;
-
+  ros::Publisher cmd_vel_pub_, target_reached_pub_;
   geometry_msgs::Twist cmd_vel_msg_;
   std_msgs::Bool target_reached_msg_;
 
-  bool target_reached_ = false;
+  Params params_;
+  geometry_msgs::Pose2D target_, pose_;
+
+  bool target_reached_ = true;
+
+private:
+  template <class T>
+  void getParam(const ros::NodeHandle& nh, const std::string& name, T* storage) const
+  {
+    if (!nh.getParam(name, *storage))
+    ROS_ERROR("Failed to get parameter \"%s\" from server\n", name.data());
+  };
+
+  template<typename T>
+  int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+  };
+
+  double euclidean_distance(double x1, double y1, double x2, double y2) {
+    tf::Vector3 v1(x1, y1, 0.0);
+    tf::Vector3 v2(x2, y2, 0.0);
+    return v1.distance(v2);
+  };
 };
+}
--- ROS_ws/src/pose_control/src/pose_control_node.cpp
+++ ROS_ws/src/pose_control/src/pose_control_node.cpp
@@ -1,23 +1,25 @@
 #include "../include/pose_control.h"
 
+using namespace PoseControlNS;
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "pose_control");
   ros::NodeHandle nh;
 
-  ros::Publisher target_reached_pub = nh.advertise<std_msgs::Bool>("target/pose/reached", 1);
   ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+  ros::Publisher target_reached_pub = nh.advertise<std_msgs::Bool>("target/pose/reached",1);
 
-  PoseControl pose_control(target_reached_pub, cmd_vel_pub);
+  PoseControl pose_control(cmd_vel_pub, target_reached_pub);
+  pose_control.loadParams(nh);
 
   ros::Subscriber target_cmd_sub = nh.subscribe("target/pose/cmd", 1, &PoseControl::targetCmdCallback, &pose_control);
   ros::Subscriber odom_sub = nh.subscribe("odom", 1, &PoseControl::odomCallback, &pose_control);
+  ros::Subscriber stop_sub = nh.subscribe("pose_control/stop", 1, &PoseControl::stopCallback, &pose_control);
 
-  ros::Rate looprate(100);
+  ros::Rate looprate(40);
   while (nh.ok())
   {
     ros::spinOnce();
-    pose_control.update();
     looprate.sleep();
   }
 }
